66bdb3bdcef5a128d325e5750a587a0be8b069b4
Vitor Mayer <vpmayer@hotmail.com>
2017-11-21 13:27:13 -0200

    add corss

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express')
  var app = express()
   
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+  
  // The code below will display 'Hello World!' to the browser when you go to http://localhost:3000
  app.get('/', function (req, res) {
    res.send('Hello World!')

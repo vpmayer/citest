fb1e70eb8f877f99d04c6c35b1650ba453b031b7
Vitor Mayer <vpmayer@hotmail.com>
2017-11-21 13:46:55 -0200

    corss

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var express = require('express')
  var app = express()
   
 
-app.use(function(req, res, next) {
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+ });
 
  // The code below will display 'Hello World!' to the browser when you go to http://localhost:3000
  app.get('/', function (req, res) {

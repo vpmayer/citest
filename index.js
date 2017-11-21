ffbf6058535924f1f437dd4989a08c7b24ad0c4d
Vitor Mayer <vpmayer@hotmail.com>
2017-11-21 13:32:18 -0200

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-  
+
  // The code below will display 'Hello World!' to the browser when you go to http://localhost:3000
  app.get('/', function (req, res) {
    res.send('Hello World!')
@@ -17,9 +17,6 @@ app.use(function(req, res, next) {
    console.log('Example app listening on port 3000!')
  })
 
-  app.listen(443, function () {
-   console.log('Example app listening on port 443!')
- })
   
  module.exports = app;
   
